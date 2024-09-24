440ec6dbed5dec9608f16ad92ab7c960a58a8797
abdicore <abdirhamanmohamed@gmail.com>
2024-09-24 11:34:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script >
+import AppHeader from './components/AppHeader.vue';
+
+
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+    AppHeader,
+  
+  }
+}
+</script>
+
+<template>
+  <AppHeader/>
+<h1>app</h1>
+<p>ciao</p>
+
+</template>
+<style lang="scss">
+@use "./style/general.scss"  as *
+
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script >
+
+
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+  
+  }
+}
+</script>
+
+<template>
+<h1>AppHeader</h1>
+
+</template>
+<style lang="scss">
+
+
+</style>
